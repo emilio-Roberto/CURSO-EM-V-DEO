f1c4b9adb79cfde87912d5b42252b19c7427b9ce
Emilio Roberto <emiliobr22@hotmail.com>
2023-12-23 23:03:38 -0300

    Scripts aula 12/desafios

---
+++ MUNDO 2/AULAS/AULA-13-(ESTRUTURA DE REPETIÇÃO FOR).py
@@ -0,0 +1,39 @@
+print('OI')
+print('OI')
+print('OI')
+
+for c in range (1,3):
+    print('OI')
+
+for c in range(0,4):
+    print(c)
+print('TCHAU')
+
+for c in range(6, 0, -1):
+    print(c)
+print('Fim')
+
+for c in range(0, 7, 2):
+    print(c)
+print('Fim')
+
+n = int(input('Digite um número: '))
+for c in range(0, n+1):
+    print(c)
+print('fim')
+
+i = int(input('Digite o início: '))
+f = int(input('Digite o fim: '))
+p = int(input('Passos: '))
+for c in range(0, f+1, p):
+    print(c)
+print('fim')
+
+for c in range(0, 3):
+    n = int(input('Digite um valor'))
+print('Fim')
+
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n
+print('O somatorio de todos os valores foi: {}'.format(s))
